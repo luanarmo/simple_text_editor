c87cf955033e0392855f2d9160b44f1ee27075df
luanarmo <ang3l_morales800@live.com>
2021-04-12 00:04:37 -0500

    first commit

---
+++ src/Editor.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import javax.swing.*;
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+
+/**
+ * A simple example showing how create an "Edit" menu from the same
+ * actions used by RSyntaxTextArea.<p>
+ *
+ * This example uses RSyntaxTextArea 3.0.5.
+ */
+public class Editor extends JFrame {
+
+    public Editor() {
+
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(40, 120);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_C);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setJMenuBar(createMenuBar(textArea));
+
+        setContentPane(cp);
+        setTitle("Code Editor");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    private static JMenuBar createMenuBar(RSyntaxTextArea textArea) {
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu editMenu = new JMenu("Edit");
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.UNDO_ACTION)));
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.REDO_ACTION)));
+        editMenu.addSeparator();
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.CUT_ACTION)));
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.COPY_ACTION)));
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.PASTE_ACTION)));
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.DELETE_ACTION)));
+        editMenu.addSeparator();
+        editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.SELECT_ALL_ACTION)));
+        menuBar.add(editMenu);
+
+        return menuBar;
+    }
+
+    private static JMenuItem createMenuItem(Action action) {
+        JMenuItem item = new JMenuItem(action);
+        item.setToolTipText(null); // Swing annoyingly adds tool tip text to the menu item
+        return item;
+    }
+
+    public static void main(String[] args) {
+        // Start all Swing applications on the EDT.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Editor().setVisible(true);
+            }
+        });
+    }
+
+}
